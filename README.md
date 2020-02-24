99da0f3ec82d18fd495da51cc585e7b639087eb6
KinuthiaN <Kinuthia26@yahoo.com>
2020-02-24 16:03:45 +0300

    Adds styles

---
+++ README.md
@@ -0,0 +1,29 @@
+# DELANI STUDIO
+It is a website that is a fictional studio
+
+## Authors
+Geoffrey Kinuthia
+
+### About
+It's a website that tells gives details on services offered by Delani Studio
+
+#### Instructions
+All you need is a computer and internet and link to the website to reach us
+
+#### Link To Github Pages
+
+
+#### License and Copyright Information
+This website is free and  open to the public.
+
+#### Author(s) Information
+My names is Geoffrey and, am a beginner in software development.
+
+#### BDD
+This website is coded in Html and is given more functionality by Javascript and jQuery
+
+#### Technologies Used
+HTML,CSS,jQuery and Javascript are the technologies used.
+
+#### Contact Information
+You can reach me through my github @KinuthiaN.
---
+++ js/script.js
@@ -0,0 +1,79 @@
+$("document").ready(function() {
+    $("div.image1,#p1").click(function() { 
+        $("#p1").toggle();
+        // $('.image1').toggle();
+    });
+});
+
+$("document").ready(function() {
+    $("div.image2,#p2").click(function() { 
+        $("#p2").toggle();
+        // $('.image2').toggle();
+    });
+});
+
+$("document").ready(function() {
+    $("div.image3,#p3").click(function() { 
+        $("#p3").toggle();
+        // $('.image3').toggle();
+    });
+});
+$(".second-image").hover(function() {
+    $(".overlay2").show();
+    $(".second-image").mouseleave(function(){
+        $(".overlay2").hide();
+    });
+});
+
+$(".first-image").hover(function() {
+    $(".overlay1").show();
+    $(".first-image").mouseleave(function(){
+        $(".overlay1").hide();
+    });
+});
+
+$(".third-image").hover(function() {
+    $(".overlay3").show();
+    $(".third-image").mouseleave(function(){
+        $(".overlay3").hide();
+    });
+});
+
+$(".fourth-image").hover(function() {
+    $(".overlay4").show();
+    $(".fourth-image").mouseleave(function(){
+        $(".overlay4").hide();
+    });
+});
+
+$(".fifth-image").hover(function() {
+    $(".overlay5").show();
+    $(".fifth-image").mouseleave(function(){
+        $(".overlay5").hide();
+    });
+});
+
+$(".sixth-image").hover(function() {
+    $(".overlay6").show();
+    $(".sixth-image").mouseleave(function(){
+        $(".overlay6").hide();
+    });
+});
+
+$(".seventh-image").hover(function() {
+    $(".overlay7").show();
+    $(".seventh-image").mouseleave(function(){
+        $(".overlay7").hide();
+    });
+});
+
+$(".eighth-image").hover(function() {
+    $(".overlay8").show();
+    $(".eighth-image").mouseleave(function(){
+        $(".overlay8").hide();
+    });
+});
+$(form).submit(function(event){
+    var name=$("#name").val();
+    alert("Thank you " + name + " for your feedback");
+});
